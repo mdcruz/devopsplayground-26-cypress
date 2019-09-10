23725cba64f1bab123d367aae300d16dd66e8aaa
mdcruz <marie.cruz1@news.co.uk>
2019-09-10 14:32:17 +0100

    feat: added tests for accessibility

--- cypress/integration/image.gallery.spec.js
+++ cypress/integration/image.gallery.spec.js
@@ -3,11 +3,14 @@
 describe('when I go to my image gallery application', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000/gallery/');
+    cy.injectAxe();
   });
 
   it('should display the title correctly', () => {
     cy.get('.header')
       .should('have.text', 'Image Gallery');
+
+    cy.checkA11y('.header');
   });
 
   it('should click on the image gallery', () => {
@@ -19,8 +22,7 @@ describe('when I go to my image gallery application', () => {
       .should('have.text', 'Dog in the Park - Marie Cruz');
   });
 
-  it('should pass basic accessibility tests', () => {
-    // IMPLEMENT ME
+  it('should pass basic accessibility test on gallery', () => {
+    cy.checkA11y('.gallery');
   });
-
 });
